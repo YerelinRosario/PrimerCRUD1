bfb1998e5b43d91f90ede1dc19f721a31a39afd0
YerelinRosario <yerelinrosario26@gmail.com>
2024-10-14 16:18:42 -0400

    Se agrega el Proyecto

---
+++ PrimerCRUD1/Models/PrimerCrudContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrimerCRUD1.Models;
+
+public partial class PrimerCrudContext : DbContext
+{
+    public PrimerCrudContext()
+    {
+    }
+
+    public PrimerCrudContext(DbContextOptions<PrimerCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+            //        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=PrimerCRUD;Trusted_Connection=True;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3213E83FEFD3AE13");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
